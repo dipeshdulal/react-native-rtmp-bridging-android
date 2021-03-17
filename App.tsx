f00875a6a18eafe988a7dd2b5fe8a4a165ab48e6
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2021-03-17 17:44:59 +0545

    feat: use rct camera view in apo

--- App.tsx
+++ App.tsx
@@ -8,21 +8,72 @@
  * @format
  */
 
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import {
+  Button,
+  View,
   SafeAreaView,
   StatusBar,
   useColorScheme,
 } from 'react-native';
-import RCTCameraView from './RCTCameraView';
+import { CameraView } from './RCTCameraView';
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
+  const cameraViewRef = useRef<CameraView>(null);
+
+  const [audioMuted, setAudioMuted] = useState<boolean>(false);
+  const [videoMuted, setVideoMuted] = useState<boolean>(false);
+  const [isFront, setIsFront] = useState<boolean>(true);
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <RCTCameraView style={{ flex: 1 }} />
+      <CameraView
+        style={{ flex: 1 }}
+        streamWidth={720}
+        streamHeight={1280}
+        ref={cameraViewRef}
+        audioMuted={audioMuted}
+        videoMuted={videoMuted}
+        camera={isFront ? "front" : "back"}
+        streamUrl="rtmp://global-live.mux.com:5222/app/" />
+      <View style={{
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        position: "absolute",
+        bottom: 0,
+        width: "100%",
+        marginBottom: 10,
+        flexWrap: "wrap"
+      }}>
+        <Button
+          title="Start"
+          onPress={() => {
+            cameraViewRef.current?.startPublish("95a9ae5a-4270-1bae-f3bb-35b9936f2de3");
+          }} />
+        <Button
+          title="Stop"
+          onPress={() => {
+            cameraViewRef.current?.stopPublish();
+          }} />
+        <Button
+          title={audioMuted ? "UM A" : "M A"}
+          onPress={() => {
+            setAudioMuted(m => !m);
+          }} />
+        <Button
+          title={videoMuted ? "UM V" : "M V"}
+          onPress={() => {
+            setVideoMuted(v => !v);
+          }} />
+
+        <Button
+          title="SC"
+          onPress={() => {
+            setIsFront(f => !f)
+          }} />
+      </View>
     </SafeAreaView>
   );
 };
