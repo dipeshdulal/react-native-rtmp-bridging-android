530e18e239229f3140f3986fb3a687270ec9fda5
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2021-03-17 17:44:40 +0545

    feat: added rct camera view manager with props and commands

--- android/app/src/main/java/com/androidbridging/RCTCameraViewManager.java
+++ android/app/src/main/java/com/androidbridging/RCTCameraViewManager.java
@@ -1,25 +1,108 @@
 package com.androidbridging;
 
-import android.content.res.Resources;
 import android.util.Log;
 import android.view.SurfaceHolder;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
+import com.androidbridging.rtmp.CustomRTMPCamera2;
+import com.facebook.infer.annotation.Assertions;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.common.MapBuilder;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.annotations.ReactProp;
 import com.pedro.encoder.input.video.CameraHelper;
-import com.pedro.rtplibrary.rtmp.RtmpCamera2;
 
 import net.ossrs.rtmp.ConnectCheckerRtmp;
+
+import java.util.Map;
 
 public class RCTCameraViewManager extends SimpleViewManager<RCTCameraView> implements SurfaceHolder.Callback {
 
     private static final String CAMERA_VIEW = "RCTCameraView";
+    private static final String START_PUBLISH_COMMAND = "startPublish";
+    private static final String STOP_PUBLISH_COMMAND = "stopPublish";
+
+    private int streamWidth = 1280;
+    private int streamHeight = 720;
+    private String streamUrl = "";
+    private boolean audioMuted = false;
+    private boolean videoMuted = false;
+    private String camera = "front";
 
     private SurfaceHolder surfaceHolder;
     private RCTCameraView cameraView;
     private CustomRTMPCamera2 rtmpCamera2;
+
+    @ReactProp(name="streamWidth")
+    public void setStreamWidth(RCTCameraView view, int streamWidth) {
+        this.streamWidth = streamWidth;
+    }
+
+    @ReactProp(name="streamHeight")
+    public void setStreamheight(RCTCameraView view, int streamHeight) {
+        this.streamHeight = streamHeight;
+    }
+
+    @ReactProp(name="streamUrl")
+    public void setStreamUrl(RCTCameraView view, String streamUrl) {
+        this.streamUrl = streamUrl;
+    }
+
+    @ReactProp(name="audioMuted")
+    public void setAudioMuted(RCTCameraView view, boolean muted) {
+        this.audioMuted = muted;
+        if (rtmpCamera2 == null) { return; }
+
+        if(this.audioMuted){
+            rtmpCamera2.disableAudio();
+        } else {
+            rtmpCamera2.enableAudio();
+        }
+
+    }
+
+    @ReactProp(name="videoMuted")
+    public void setVideoMuted(RCTCameraView view, boolean muted) {
+        this.videoMuted = muted;
+        if (rtmpCamera2 == null){return;}
+
+        if(this.videoMuted) {
+            cameraView.muteVideo();
+        } else {
+            cameraView.unMuteVideo();
+        }
+
+    }
+
+    @ReactProp(name="camera")
+    public void setCamera(RCTCameraView view, String camera) {
+        this.camera = camera;
+        if(rtmpCamera2 != null){
+            if (rtmpCamera2.isFrontCamera() && camera.equals("back")) {
+                rtmpCamera2.switchCamera();
+            }
+            if (!rtmpCamera2.isFrontCamera() && camera.equals("front")) {
+                rtmpCamera2.switchCamera();
+            }
+        }
+    }
+
+    @Override
+    public void receiveCommand(@NonNull RCTCameraView root, String commandId, @Nullable ReadableArray args) {
+        Log.d("receiving commands: ", "receiveCommand: "+ commandId + "  " + args.toString());
+        switch (commandId) {
+            case START_PUBLISH_COMMAND:
+                this.startPublish(args.getString(0));
+                break;
+            case STOP_PUBLISH_COMMAND:
+                this.stopPublish();
+                break;
+            default:
+        }
+    }
 
     @NonNull
     @Override
@@ -75,10 +158,31 @@ public class RCTCameraViewManager extends SimpleViewManager<RCTCameraView> imple
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
         Log.d("RNCameraView", "surfaceChanged");
-        rtmpCamera2.startPreview(CameraHelper.Facing.BACK);
-        if(rtmpCamera2.prepareAudio() && rtmpCamera2.prepareVideo(1280, 720, 30, 120*1024, 0, 90)){
-            rtmpCamera2.startStream("rtmp://global-live.mux.com:5222/app/105c61cd-26c4-5fd1-4186-37490351b2eb");
+        rtmpCamera2.startPreview(CameraHelper.Facing.FRONT, streamHeight, streamWidth);
+    }
+
+    public void startPublish(String streamKey) {
+        if(rtmpCamera2.prepareAudio() && rtmpCamera2.prepareVideo(streamHeight, streamWidth, 30, 20*1024, 2, 90)){
+            rtmpCamera2.startStream(streamUrl+streamKey);
+
+            if(this.audioMuted){
+                rtmpCamera2.disableAudio();
+            } else {
+                rtmpCamera2.enableAudio();
+            }
+
+            if(this.videoMuted) {
+                rtmpCamera2.pauseRecord();
+            } else {
+                rtmpCamera2.resumeRecord();
+            }
+
         }
+
+    }
+
+    public void stopPublish(){
+        rtmpCamera2.stopStream();
     }
 
     @Override
